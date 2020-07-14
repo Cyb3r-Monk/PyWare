12654e30f444021ce9ef56fb62f620458ee526cf
Cyb3rM0nk3y <mehmetergene@gmail.com>
2020-07-14 20:05:20 +0200

    Update make_keys.py

--- make_keys.py
+++ make_keys.py
@@ -7,7 +7,7 @@ from Crypto.PublicKey import RSA
 raw_key = RSA.generate(2048)
 private_key = raw_key.exportKey('PEM')
 try:
-    with open('master_private.pem', 'w+') as keyfile:
+    with open('private_key.pem', 'w+') as keyfile:
         keyfile.write(private_key)
         keyfile.close()
     print "[*] Successfully created your local RSA private key"
@@ -17,9 +17,13 @@ except:
 make_public = raw_key.publickey()
 public_key = make_public.exportKey('PEM')
 try:
-    with open("master_public.pem", "w+") as keyfile:
+    with open("public_key.pem", "w+") as keyfile:
         keyfile.write(public_key)
         keyfile.close()
+    #delete RSA data from memory
+    raw_key = "ThisWasTheRawRSAKey"
+    private_key = "ThisWasTheRSAPrivateKey"
     print "[*] Successfully created your local RSA public key"
 except:
     print "[*] Error creating your key"
+    
